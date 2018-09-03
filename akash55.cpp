befee957fc9eeb83d0793698e77277cd00dfe23a
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-09-03 17:48:16 +0530

    Add files via upload

---
+++ akash55.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n1, remainder;
+cout << "wheather the number is even or odd";
+cout << "enter a number";
+cin >> n1;
+remainder = n1/2;
+if(remainder = 0)
+{
+cout << "number" <<n1<< "is even";
+}
+else
+{
+cout << "number" <<n1<< "is odd";
+}
+return 0;
+}
